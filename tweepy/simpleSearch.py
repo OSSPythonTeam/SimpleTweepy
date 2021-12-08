98bd6dc4140c69ae65c50e8be17eb0dcbf6520e3
sectionr0 <sectionr0@gmail.com>
2021-12-08 16:45:33 +0900

    Update simpleSearch.py
    개수를 조절해서 출력할 수 있도록 수정

--- tweepy/simpleSearch.py
+++ tweepy/simpleSearch.py
@@ -1,11 +1,11 @@
+import tweepy
+
+
 def simple_tweet_result(api,keyword):
     result = []  # 크롤링 텍스트를 저장 할 리스트 변수
 
-    for i in range(1, 7):  # 1,2 페이지 크롤링
+    for tweet in tweepy.Cursor(api.search_tweets, keyword).items(30):
         # keyword 검색 실시. 결과가 tweets 변수에 담긴다.
-        tweets = api.search_tweets(keyword)
-        result = []
-        for tweet in tweets:
 
             result.append("ID : " + tweet.author.name)
             result.append([tweet.text])
